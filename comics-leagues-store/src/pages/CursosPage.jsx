961f26d281e1cc2f87bdb542aeb40d488d38e7a0
atuning120 <90119017+atuning120@users.noreply.github.com>
2024-10-29 19:58:29 -0300

    afasdfasd

--- comics-leagues-store/src/pages/CursosPage.jsx
+++ comics-leagues-store/src/pages/CursosPage.jsx
@@ -105,35 +105,35 @@ const CursosPage = () => {
         {/* Filtrar por cursos*/}
         <label htmlFor="courseFilter" className=" p-4 text-lg font-medium text-gray-700">
           Filtrar por Curso:
-        <select
-          id="courseFilter"
-          value={selectedCourse}
-          onChange={handleFilterChange}
-          className="mt-1 block  pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-        >
-          <option value="">Todos los cursos</option>
-          {products.map((product) => (
-            <option key={product._id} value={product.nombre}>
-              {product.nombre}
-            </option>
-          ))}
-        </select>
+          <select
+            id="courseFilter"
+            value={selectedCourse}
+            onChange={handleFilterChange}
+            className="mt-1 block  pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          >
+            <option value="">Todos los cursos</option>
+            {products.map((product) => (
+              <option key={product._id} value={product.nombre}>
+                {product.nombre}
+              </option>
+            ))}
+          </select>
         </label>
 
         {/* Filtrar por Categoria*/}
         <label htmlFor="categoryFilter" className="p-4 text-lg font-medium text-gray-700">
           Filtrar por Categoria:
           <select
-          id="categoryFilter"
-          value={selectedCategory}
-          onChange={handleCategoryChange}
-          className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            id="categoryFilter"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+            className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
           >
             <option value="">Todas las categorias</option>
-              {[...new Set(products.map((product) => product.categoria))].map((categoria) => (
-            <option key={categoria} value={categoria}>
-              {categoria}
-            </option>
+            {[...new Set(products.map((product) => product.categoria))].map((categoria) => (
+              <option key={categoria} value={categoria}>
+                {categoria}
+              </option>
             ))}
           </select>
         </label>
@@ -142,16 +142,16 @@ const CursosPage = () => {
         <label htmlFor="levelFilter" className="p-4 text-lg font-medium text-gray-700">
           Filtrar por Nivel:
           <select
-          id="levelFilter"
-          value={selectedLevel}
-          onChange={handleLevelChange}
-          className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            id="levelFilter"
+            value={selectedLevel}
+            onChange={handleLevelChange}
+            className="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
           >
             <option value="">Todos los niveles</option>
-              {[...new Set(products.map((product) => product.nivel))].map((nivel) => (
-            <option key={nivel} value={nivel}>
-              {nivel}
-            </option>
+            {[...new Set(products.map((product) => product.nivel))].map((nivel) => (
+              <option key={nivel} value={nivel}>
+                {nivel}
+              </option>
             ))}
           </select>
         </label>
@@ -159,57 +159,57 @@ const CursosPage = () => {
         <label htmlFor="levelFilter" className="p-4 text-lg font-medium text-gray-700">
           Filtrar por precio:
           <select
-          id="sortOrder"
-          value={sortOrder}
-          onChange={handleSortOrderChange}
-          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-        >
-          <option value="">Sin ordenar</option>
-          <option value="priceAsc">Precio Ascendente</option>
-          <option value="priceDesc">Precio Descendente</option>
-        </select>
+            id="sortOrder"
+            value={sortOrder}
+            onChange={handleSortOrderChange}
+            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          >
+            <option value="">Sin ordenar</option>
+            <option value="priceAsc">Precio Ascendente</option>
+            <option value="priceDesc">Precio Descendente</option>
+          </select>
         </label>
       </div>
 
       {/* Mostrar cursos filtrados */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-  {filteredProducts.map((product) => (
-    <div 
-      key={product.id} 
-      className="bg-white rounded-lg shadow-md p-4 flex flex-col h-full"
-    >
-      {/* Contenido superior */}
-      <div className="flex-grow">
-        <Image
-          src={product.imagen}
-          alt={product.nombre}
-          className="w-full h-48 object-cover rounded-md mb-4"
-        />
-        <h3 className="text-lg font-semibold">{product.nombre}</h3>
-        <p className="text-gray-600">{product.descripcion}</p>
-        <p className="text-gray-600">
-          {product.categoria} - {product.nivel}
-        </p>
-      </div>
+        {filteredProducts.map((product) => (
+          <div
+            key={product.id}
+            className="bg-white rounded-lg shadow-md p-4 flex flex-col h-full"
+          >
+            {/* Contenido superior */}
+            <div className="flex-grow">
+              <Image
+                src={product.imagen}
+                alt={product.nombre}
+                className="w-full h-48 object-cover rounded-md mb-4"
+              />
+              <h3 className="text-lg font-semibold">{product.nombre}</h3>
+              <p className="text-gray-600">{product.descripcion}</p>
+              <p className="text-gray-600">
+                {product.categoria} - {product.nivel}
+              </p>
+            </div>
 
-      {/* Contenedor de precio y botón, siempre alineados al fondo */}
-      <div className="mt-auto">
-        <div className="mt-4">
-          <span className="text-xl font-bold">
-            ${product.precio.toLocaleString('es-ES')}
-          </span>
-        </div>
-        <button
-          onClick={() => AddCarritoFunc(product.id)}
-          className="mt-2 w-full bg-purple-600 text-white py-2 px-4 rounded hover:bg-purple-700 transition duration-300"
-          disabled={loadingProduct === product.id}
-        >
-          {loadingProduct === product.id ? 'Añadiendo...' : 'Añadir al Carrito'}
-        </button>
+            {/* Contenedor de precio y botón, siempre alineados al fondo */}
+            <div className="mt-auto">
+              <div className="mt-4">
+                <span className="text-xl font-bold">
+                  ${product.precio.toLocaleString('es-ES')}
+                </span>
+              </div>
+              <button
+                onClick={() => AddCarritoFunc(product.id)}
+                className="mt-2 w-full bg-purple-600 text-white py-2 px-4 rounded hover:bg-purple-700 transition duration-300"
+                disabled={loadingProduct === product.id}
+              >
+                {loadingProduct === product.id ? 'Añadiendo...' : 'Añadir al Carrito'}
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  ))}
-</div>
 
     </div>
   );
